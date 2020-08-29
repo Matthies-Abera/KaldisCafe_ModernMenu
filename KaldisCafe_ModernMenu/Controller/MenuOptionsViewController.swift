267c5adc35e183f604f6d50d732350d6aabd4b9c
Matthies Abera <s3779748@student.rmit.edu.au>
2020-08-30 01:23:19 +1000

    Create Welcome, Menu and View Menu Views

---
+++ KaldisCafe_ModernMenu/Controller/MenuOptionsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MenuOptionsViewController.swift
+//  KaldisCafe_ModernMenu
+//
+//  Created by Matthies Abera on 30/8/20.
+//  Copyright © 2020 Thinking Bear Solutions. All rights reserved.
+//
+
+import UIKit
+
+class MenuOptionsViewController: UIViewController {
+
+    @IBAction func viewMenuPressed(_ sender: RoundButton) {
+        self.performSegue(withIdentifier: "goToViewMenu", sender: self)
+    }
+    
+    @IBAction func addMenuItemPressed(_ sender: RoundButton) {
+    }
+    
+    @IBAction func editMenuItemPressed(_ sender: RoundButton) {
+    }
+    
+    @IBAction func deleteMenuItemPressed(_ sender: RoundButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ KaldisCafe_ModernMenu/View/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  KaldisCafe_ModernMenu
+//
+//  Created by Matthies Abera on 29/8/20.
+//  Copyright © 2020 Thinking Bear Solutions. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+        self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
